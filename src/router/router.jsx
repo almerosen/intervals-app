4d8a5701d363c2d222ff1c87fbfbc7a1b6b125e2
Kristofer_Almeros <kristofer.almeros@hotmail.com>
2024-10-17 14:21:23 +0200

    added base to vite config

---
+++ src/router/router.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import { LoadingScreen } from "../components/LoadingScreen/LoadingScreen";
+import { SetTimer } from "../components/SetTimer/SetTimer";
+import { DigitalTimer } from "../components/DigitalTimer/DigitalTimer";
+import { AlarmView } from "../components/AlarmView/AlarmView";
+import { PausScreen } from "../components/PausScreen/PausScreen";
+
+const router = createBrowserRouter([
+    {
+        path: "/", 
+        element: <LoadingScreen />
+    },
+    {
+        path: "/setTimer",
+        element: <SetTimer />
+    },
+    {
+        path: "digital-timer",
+        element: <DigitalTimer />
+    },
+    {
+        path: "alarm-view",
+        element: <AlarmView />
+    },
+    {
+        path: "paus",
+        element: <PausScreen />
+    }
+])
+
+export default router
