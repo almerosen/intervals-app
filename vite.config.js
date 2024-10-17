b7a55e9bb70afadb125dd0859107729dc0451635
Kristofer_Almeros <kristofer.almeros@hotmail.com>
2024-10-17 14:33:11 +0200

    changes to base

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log(process.env.NODE_ENV)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/live-17-okt/",
+  base: process.env.NODE_ENV === 'production' ? "/live-17-okt/" : "/",
   // server: {
   //   host: "0.0.0.0",
   //   port: 5173
